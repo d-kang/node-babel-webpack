74b1494924b9ebba6203026050982bde1329f6be
d-kang <david.kang714@gmail.com>
2017-10-13 19:21:01 -0700

    Fix Npm Start Script

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3005;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -13,7 +13,7 @@ app.use(morgan('dev'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
-  res.send('Hello World Test!!~')
+  res.send('Hello World Test!!!!~')
 })
 
 // app.use('/', 'router');
